9084e9dd4a7a52fbb88a68cdc2c7544ac77babe0
kaiwiking <130120737+kaiwiking@users.noreply.github.com>
2023-04-10 09:59:28 +0300

    Delete src directory

--- src/Arabic.java
+++
@@ -1,9 +0,0 @@
-public class Arabic {
-    int toDecimal(String ar_num){ return Integer.parseInt(ar_num); }
-    String add(String num1, String num2){ return String.valueOf((toDecimal(num1) + toDecimal(num2))); }
-    String sub(String num1, String num2){
-        return String.valueOf(toDecimal(num1) - toDecimal(num2));
-    }
-    String mul(String num1, String num2){ return String.valueOf(toDecimal(num1) * toDecimal(num2)); }
-    String divs(String num1, String num2){ return String.valueOf((toDecimal(num1) / toDecimal(num2))); }
-}
